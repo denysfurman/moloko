6f0f8e7fae8301231df491bbad81737a2788e53c
denis <furik1102@gmail.com>
2018-04-24 12:34:08 +0300

    menu

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,30 @@
-$(function() {
 
-	// Custom JS
 
-});
+    //  mobile menu
+    jQuery(document).ready(function($){
+        var $lateral_menu_trigger = $('#cd-menu-trigger'),
+            $content_wrapper = $('.cd-main-content'),
+            $navigation = $('header');
+
+        //open-close lateral menu clicking on the menu icon
+        $lateral_menu_trigger.on('click', function(event){
+            event.preventDefault();
+
+            $lateral_menu_trigger.toggleClass('is-clicked');
+            $navigation.toggleClass('lateral-menu-is-open');
+            $content_wrapper.toggleClass('lateral-menu-is-open').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+                // firefox transitions break when parent overflow is changed, so we need to wait for the end of the trasition to give the body an overflow hidden
+                $('body').toggleClass('overflow-hidden');
+            });
+            $('#cd-lateral-nav').toggleClass('lateral-menu-is-open');
+            $('body').toggleClass('shadow');
+            $('.cd-navigation li').toggleClass('inner');
+
+            //check if transitions are not supported - i.e. in IE9
+            if($('html').hasClass('no-csstransitions')) {
+                $('body').toggleClass('overflow-hidden');
+            }
+        });
+    });
+
+
--- gulpfile.js
+++ gulpfile.js
@@ -18,7 +18,7 @@ gulp.task('browser-sync', function() {
 		server: {
 			baseDir: 'app'
 		},
-		notify: false,
+		notify: false
 		// open: false,
 		// tunnel: true,
 		// tunnel: "projectname", //Demonstration page: http://projectname.localtunnel.me
@@ -38,7 +38,7 @@ gulp.task('styles', function() {
 gulp.task('js', function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
-		'app/js/common.js', // Always at the end
+		'app/js/common.js' // Always at the end
 		])
 	.pipe(concat('scripts.min.js'))
 	// .pipe(uglify()) // Mifify js (opt.)
