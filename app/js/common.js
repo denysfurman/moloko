bdb13377805ddae5f239001a436e9adad7b4e330
denis <furik1102@gmail.com>
2018-04-24 13:31:24 +0300

    search

--- app/js/common.js
+++ app/js/common.js
@@ -43,6 +43,17 @@
 
         })
 
+        function expand() {
+            $(".search").toggleClass("close");
+            $(".input").toggleClass("square");
+            if ($('.search').hasClass('close')) {
+                $('input').focus();
+            } else {
+                $('input').blur();
+            }
+        }
+        $('button').on('click', expand);
+
     });
 
 
--- gulpfile.js
+++ gulpfile.js
@@ -38,7 +38,7 @@ gulp.task('styles', function() {
 gulp.task('js', function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
-		'app/js/common.js' // Always at the end
+
 		])
 	.pipe(concat('scripts.min.js'))
 	// .pipe(uglify()) // Mifify js (opt.)
