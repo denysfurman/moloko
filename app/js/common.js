83591b0c43f465515aaa74c9adb81df7e6e1beb1
denys furman <furik1102@gmail.com>
2018-05-24 22:37:23 +0300

    filter

--- app/js/common.js
+++ app/js/common.js
@@ -61,6 +61,42 @@
             $('body').removeClass('search-active');
             $('.icon-close').fadeOut();
         });
+     //date
+        var $dates = $('#from, #to').datepicker();
+
+        $('#clear-dates').on('click', function () {
+            $dates.datepicker('setDate', null);
+        });
+        $.datepicker.setDefaults( $.datepicker.regional[ "ru" ] );
+
+        $("#datepicker").datepicker({
+            inline: true,
+            changeYear: true,
+            changeMonth: true
+        });
+
+        jQuery(function ($) {
+            $.datepicker.regional['ru'] = {
+                closeText: 'Закрыть',
+                prevText: '&#x3c;Пред',
+                nextText: 'След&#x3e;',
+                currentText: 'Сегодня',
+                monthNames: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь',
+                    'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+                monthNamesShort: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь',
+                    'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+                dayNames: ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота'],
+                dayNamesShort: ['вск', 'пнд', 'втр', 'срд', 'чтв', 'птн', 'сбт'],
+                dayNamesMin: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+                weekHeader: 'Нед',
+                dateFormat: 'dd.mm.yy',
+                firstDay: 1,
+                isRTL: false,
+                showMonthAfterYear: false,
+                yearSuffix: ''
+            };
+            $.datepicker.setDefaults($.datepicker.regional['ru']);
+        });
 
 
 
